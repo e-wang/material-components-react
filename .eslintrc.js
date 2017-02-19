fe3a8dc98b442825ce17afb31c6c6847d0cd761f
Eric Wang <ericwan@alumni.upenn.edu>
2017-02-18 22:20:36 -0500

    Reformat .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-    "extends": [
-        "airbnb"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "import/no-extraneous-dependencies": ["error", {
-            "devDependencies": [
-                "**/*.stories.js"
-            ]
-        }],
-        "import/order": ["error", {
-            "newlines-between": "always"
-        }],
-        "react/jsx-filename-extension": ["error", {
-            "extensions": [".js"]
-        }],
-        "react/jsx-max-props-per-line": "error",
-        "react/require-default-props": "off"
-    }
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: [
+        '**/*.stories.js',
+      ],
+    }],
+    'import/order': ['error', {
+      'newlines-between': 'always',
+    }],
+    'react/jsx-filename-extension': ['error', {
+      extensions: ['.js'],
+    }],
+    'react/jsx-max-props-per-line': 'error',
+    'react/require-default-props': 'off',
+  },
 };
+
